d782b31a2b63cfc2ba4cbbcdca8111c5670dd152
lamb97 <3297892150@qq.com>
2024-06-29 21:32:52 +0800

    Update

--- DQN.py
+++ DQN.py
@@ -35,7 +35,7 @@ class Qnet(torch.nn.Module):
 
 class DQN:
     "DQN算法"
-    def __init__(self, state_dim, hidden_dim, action_dim, learning_rate, gamma, epslion, target_update, device):
+    def __init__(self, state_dim, hidden_dim, action_dim, learning_rate, gamma, epsilon, target_update, device):
         self.action_dim = action_dim
         self.q_net = Qnet(state_dim,hidden_dim,self.action_dim).to(device)   #Q网络
 
@@ -45,12 +45,12 @@ class DQN:
         #使用Adam优化器
         self.optimizer = torch.optim.Adam(self.q_net.parameters(), lr = learning_rate)
         self.gamma = gamma  #折扣因子
-        self.epslion = epslion  #epslion贪婪策略
+        self.epsilon = epsilon  #epslion贪婪策略
         self.target_update = target_update  #目标网络更新频率
         self.count = 0  #计数器，记录更新次数
         self.device = device
     def take_action(self,state): #eplion-贪婪策略采取动作
-        if np.random.random() < self.epslion:
+        if np.random.random() < self.epsilon:
             action = np.random.randint(self.action_dim)
         else:
             state = torch.tensor([state], dtype=torch.float).to(self.device)
@@ -77,3 +77,75 @@ class DQN:
             self.target_q_net.load_state_dict(self.q_net.state_dict()) #更新目标网络
         self.count += 1
 
+
+
+if __name__ == '__main__':
+    lr = 2e-3
+    num_episode = 500
+    hidden_dim = 128
+    gamma = 0.98
+    epslion = 0.01
+    target_update = 10
+    buffer_size = 10000
+    minimal_size = 500
+    batch_size = 64
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    env_name = 'CartPole-v0'
+    env = gym.make(env_name)
+    random.seed(0)
+    np.random.seed(0)
+    env.seed(0)
+    torch.manual_seed(0)
+    replay_buffer = ReplayBuffer(buffer_size)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epslion, target_update, device)
+
+    return_list = []
+    for i in range(10):
+        with tqdm(total=int(num_episode / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episode / 10)):
+                episode_return = 0
+                state = env.reset()
+                done = False
+                while not done:
+                    action = agent.take_action(state)
+                    next_state, reward, done, _ = env.step(action)
+                    replay_buffer.add(state, action, reward, next_state, done)
+                    state = next_state
+                    episode_return += reward
+                    # 当buffer数据的数量超过一定值后,才进行Q网络训练
+                    if replay_buffer.size() > minimal_size:
+                        b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                        transition_dict = {
+                            'states': b_s,
+                            'actions': b_a,
+                            'next_states': b_ns,
+                            'rewards': b_r,
+                            'dones': b_d
+                        }
+                        agent.update(transition_dict)
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:
+                    pbar.set_postfix({
+                        'episode':
+                            '%d' % (num_episode / 10 * i + i_episode + 1),
+                        'return':
+                            '%.3f' % np.mean(return_list[-10:])
+                    })
+                pbar.update(1)
+
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('DQN on {}'.format(env_name))
+    plt.show()
+
+    mv_return = rl_utils.moving_average(return_list, 9)
+    plt.plot(episodes_list, mv_return)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('DQN on {}'.format(env_name))
+    plt.show()
