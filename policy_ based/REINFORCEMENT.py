7e5b21af568abff48500fc542fa6a2a8e09f9770
lamb97 <3297892150@qq.com>
2024-07-02 09:51:05 +0800

    Update

--- policy_ based/REINFORCEMENT.py
+++ policy_ based/REINFORCEMENT.py
@@ -1,7 +1,7 @@
 import gym
 import torch
 import torch.nn.functional as F
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import rl_utils
@@ -46,4 +46,59 @@ class REINFORCE:
             loss = -log_prob * G #每一步的损失函数
             loss.backward() #反向传播计算梯度
         self.optimizer.step() #梯度下降
+
+if __name__ == '__main__':
+    learning_rate = 1e-3
+    num_episodes = 1000
+    hidden_dim = 128
+    gamma = 0.98
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
+        "cpu")
+
+    env_name = "CartPole-v0"
+    env = gym.make(env_name)
+    env.seed(0)
+    torch.manual_seed(0)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    agent = REINFORCE(state_dim, hidden_dim, action_dim, learning_rate, gamma,
+                      device)
+
+    return_list = []
+    for i in range(10):
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):
+                episode_return = 0
+                transition_dict = {
+                    'states': [],
+                    'actions': [],
+                    'next_states': [],
+                    'rewards': [],
+                    'dones': []
+                }
+                state = env.reset()
+                done = False
+                while not done:
+                    action = agent.take_action(state)
+                    next_state, reward, done, _ = env.step(action)
+                    transition_dict['states'].append(state)
+                    transition_dict['actions'].append(action)
+                    transition_dict['next_states'].append(next_state)
+                    transition_dict['rewards'].append(reward)
+                    transition_dict['dones'].append(done)
+                    state = next_state
+                    episode_return += reward
+                return_list.append(episode_return)
+                agent.update(transition_dict)
+                if (i_episode + 1) % 10 == 0:
+                    pbar.set_postfix({
+                        'episode':
+                            '%d' % (num_episodes / 10 * i + i_episode + 1),
+                        'return':
+                            '%.3f' % np.mean(return_list[-10:])
+                    })
+                pbar.update(1)
+
+
+
 
