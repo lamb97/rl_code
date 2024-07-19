a89a2933e1cf82b6803060a9545ae6070575f403
lamb97 <3297892150@qq.com>
2024-07-19 15:35:04 +0800

    Update

--- TRPO.py
+++ TRPO.py
@@ -26,6 +26,19 @@ class ValueNet(torch.nn.Module):
         x = F.relu(self.fc1(x))
         return self.fc2(x)
 
+
+def compute_advantage(gamma, lmbda, td_delta):
+    td_delta = td_delta.detach().numpy()
+    advantage_list = []
+    advantage = 0.0
+    for delta in td_delta[::-1]:
+        advantage = gamma * lmbda * advantage + delta
+        advantage_list.append(advantage)
+    advantage_list.reverse()
+    return torch.tensor(advantage_list, dtype=torch.float)
+
+
+
 class TRPO:
     def __init__(self, hidden_dim, state_space, action_space, lmbda, kl_constraint, alpha, critic_lr, gamma, device):
         state_dim = state_space.shape[0]
@@ -62,8 +75,101 @@ class TRPO:
         grad2_vector = torch.cat([grad.view(-1) for grad in grad2])
         return grad2_vector
 
-    def conjugate_gradient(self, grad, states, old_action_dists):  #共轭梯度法求解方程
+    def conjugate_gradient(self, grad, states, old_action_dists):  # 共轭梯度法求解方程
         x = torch.zeros_like(grad)
+        r = grad.clone()
+        p = grad.clone()
+        rdotr = torch.dot(r, r)
+        for i in range(10):  # 共轭梯度主循环
+            Hp = self.hessian_matrix_vector_product(states, old_action_dists,
+                                                    p)
+            alpha = rdotr / torch.dot(p, Hp)
+            x += alpha * p
+            r -= alpha * Hp
+            new_rdotr = torch.dot(r, r)
+            if new_rdotr < 1e-10:
+                break
+            beta = new_rdotr / rdotr
+            p = r + beta * p
+            rdotr = new_rdotr
+        return x
+
+    def compute_surrogate_obj(self, states, actions, advantage, old_log_probs, actor):
+        log_probs = torch.log(actor(states).gather(1,actions))
+        ration = torch.exp(log_probs - old_log_probs)
+        return torch.mean(ration * advantage)
+
+    def line_search(self, states, actions, advantage, old_log_probs,
+                    old_action_dists, max_vec): # 线性搜索
+        old_para = torch.nn.utils.convert_parameters.parameters_to_vector(self.actor.parameters())
+        old_obj = self.compute_surrogate_obj(states, actions, advantage, old_log_probs, self.actor)
+
+        for i in range(15): #线性搜索主循环
+            coef = self.alpha**i
+            new_para = old_para + coef * max_vec
+            new_actor = copy.deepcopy(self.actsor)
+            torch.nn.utils.convert_parameters.vector_to_parameters(new_para, new_actor.parameters())
+            new_action_dists = torch.distributions.Categorical(new_actor(states))
+            kl_div = torch.mean(torch.distributions.kl.kl_divergence(old_action_dists, new_action_dists))
+            new_obj = self.compute_surrogate_obj(states, actions, advantage, old_log_probs, new_actor)
+            if new_obj > old_obj and kl_div < self.kl_constraint:
+                return new_obj
+            return old_obj
+
+    def policy_learn(self, states, actions, old_action_dists, old_log_probs,
+                     advantage):  # 更新策略函数
+        surrogate_obj = self.compute_surrogate_obj(states, actions, advantage,
+                                                   old_log_probs, self.actor)
+        grads = torch.autograd.grad(surrogate_obj, self.actor.parameters())
+        obj_grad = torch.cat([grad.view(-1) for grad in grads]).detach()
+        # 用共轭梯度法计算x = H^(-1)g
+        descent_direction = self.conjugate_gradient(obj_grad, states,
+                                                    old_action_dists)
+
+        Hd = self.hessian_matrix_vector_product(states, old_action_dists,
+                                                descent_direction)
+        max_coef = torch.sqrt(2 * self.kl_constraint /
+                              (torch.dot(descent_direction, Hd) + 1e-8))
+        new_para = self.line_search(states, actions, advantage, old_log_probs,
+                                    old_action_dists,
+                                    descent_direction * max_coef)  # 线性搜索
+        torch.nn.utils.convert_parameters.vector_to_parameters(
+            new_para, self.actor.parameters())  # 用线性搜索后的参数更新策略
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'],
+                              dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(
+            self.device)
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(self.device)
+        td_target = rewards + self.gamma * self.critic(next_states) * (1 -
+                                                                       dones)
+        td_delta = td_target - self.critic(states)
+        advantage = compute_advantage(self.gamma, self.lmbda,
+                                      td_delta.cpu()).to(self.device)
+        old_log_probs = torch.log(self.actor(states).gather(1,
+                                                            actions)).detach()
+        old_action_dists = torch.distributions.Categorical(
+            self.actor(states).detach())
+        critic_loss = torch.mean(
+            F.mse_loss(self.critic(states), td_target.detach()))
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()  # 更新价值函数
+        # 更新策略函数
+        self.policy_learn(states, actions, old_action_dists, old_log_probs,
+                          advantage)
+
+
+
+
+
+
 
 
 
